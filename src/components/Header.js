b378cffb86ff0fb93e4d230b91411fcca359a7be
JoshuaJHaines <joshjhaines3@gmail.com>
2021-10-28 13:18:24 -0400

    adding header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return <div className="App-header">
+            <h1 />
+            My Header
+        </div>;
+    }
+}
+
+export default Header
