c9a1e15e1c7c0c13198a42b452d2bf802519e756
JoshuaJHaines <joshjhaines3@gmail.com>
2021-10-28 13:22:19 -0400

    header and footer on

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 /****************
  * IMPORT ITEMS *
  ****************/
-import React, { Component } from 'react'
-import './App.css';
-import Message from "./components/Message"
-
-
+import React, { Component } from "react";
+import "./App.css";
+import Message from "./components/Message";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
 /********************
  * EXPORT COMPONENT *
  ********************/
 export class App extends Component {
-  render() {
-    return (
-		<div>
-			<Message name="class" message="how is your day going?" />
-			<Message name="Josh" message="did you eat yet?" />
-			<Message name="Pak" message="how was your weekend?" />
-			<Message name="Merp" message="PO-TAY-TOES!!!" />
-		</div>
-	);
-  }
+	render() {
+		return (
+      <div>
+        <Header />				
+        <Footer />
+			</div>
+		);
+	}
 }
 
-export default App
-
-
+export default App;
