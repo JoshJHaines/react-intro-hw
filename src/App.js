f342bda78a9384e48ad6dedb66066aca13856230
JoshuaJHaines <joshjhaines3@gmail.com>
2021-10-28 12:08:03 -0400

    added a Message.js file to call into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+/****************
+ * IMPORT ITEMS *
+ ****************/
 import React, { Component } from 'react'
 import './App.css';
+import Message from "./components/Message"
 
+
+/********************
+ * EXPORT COMPONENT *
+ ********************/
 export class App extends Component {
   render() {
     return (
       <div>
-        
+        Hi Josh
+        <Message />
       </div>
     )
   }
---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Message extends Component {
+    render() {
+        return (
+            <div>
+                Hello Class
+            </div>
+        )
+    }
+}
+
+export default Message
