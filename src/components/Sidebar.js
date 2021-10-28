bf20ae18bd9a815b7a90f42bd0cb492e4b6bdbe5
JoshuaJHaines <joshjhaines3@gmail.com>
2021-10-28 13:17:10 -0400

    adding sidebar file

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Sidebar extends Component {
+    render() {
+        return (
+            <div className="App-sidebar">
+                sidebar
+            </div>
+        )
+    }
+}
+
+export default Sidebar
